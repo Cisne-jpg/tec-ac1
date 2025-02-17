e1ada9ab055b92c673d623646a168c417c350e60
Cisne-jpg <a01643154@tec.mx>
2025-02-16 20:14:24 -0600

    cuadricula

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
-import Image from "next/image";
-import Rectangle from "./components/Rectangle";
 import Link from "next/link";
+import Rectangle from "./components/Rectangle";
 
-export default function Home() {
-  return (
-    <div className = "min-h-screen flex flex-col items-center justify-center gap-4">
-      <Rectangle color ="green" />
-      <Link href="/page2" className="text-blue-500 hover:underline">
-      Go to Page 2
-      </Link>
-    </div>
-  );
+export default function Page2() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-white gap-6">
+        <div className="grid grid-cols-3 gap-2">
+          <Rectangle color="red" />
+          <Rectangle color="blue" />
+          <Rectangle color="red" />
+          <Rectangle color="blue" />
+          <Rectangle color="red" />
+          <Rectangle color="blue" />
+          <Rectangle color="red" />
+          <Rectangle color="blue" />
+          <Rectangle color="red" />
+        </div>
+        <Link href="/page2" className="text-blue-500 hover:underline">
+          Go to Page 2
+        </Link>
+      </div>
+    );
 }
--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -1,14 +1,23 @@
-import { link } from "fs";
-import Image from "next/image";
 import Link from "next/link";
+import Rectangle from "../components/Rectangle";
 
 export default function Page2() {
-  return (
-    <div className = "min-h-screen flex items-center justify-center">
-      <Link href="/" className="text-blue-500 hover:underline">
-  Go back to home
-  </Link>
-      <div className="w-64 h-32 bg-green-500"></div>
-    </div>
-  );
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-white gap-6">
+        <div className="grid grid-cols-3 gap-2">
+          <Rectangle color="blue" />
+          <Rectangle color="red" />
+          <Rectangle color="blue" />
+          <Rectangle color="red" />
+          <Rectangle color="blue" />
+          <Rectangle color="red" />
+          <Rectangle color="blue" />
+          <Rectangle color="red" />
+          <Rectangle color="blue" />
+        </div>
+        <Link href="/" className="text-blue-500 hover:underline">
+          Go to Home
+        </Link>
+      </div>
+    );
 }
